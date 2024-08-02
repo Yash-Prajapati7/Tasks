57b9b5b21866bdd8d57c5f35a2b230f2ea23f3b9
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-08-02 20:04:21 +0530

    added DatabaseConnection.js

---
+++ Task3/DatabaseConnection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/Hogwarts`);
+        console.log("Database successfully connected!");
+    }
+    catch(error) {
+        console.log("MongoDB Connection Failed :(");
+        process.exit(1);
+    }
+}
+export default connectDB;
